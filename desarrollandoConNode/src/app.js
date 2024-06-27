dc9c2f7f85f20546c37a80c63b62d790becd329f
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-26 22:34:33 -0500

    exports, package.json,instalacion de dependencias nodemon

---
+++ desarrollandoConNode/src/app.js
@@ -0,0 +1,7 @@
+//import template from './js-foundation/01-template';
+
+const emailTemplate = require('./js-foundation/01-template');
+
+console.log(emailTemplate)
+//console.log('llamando  al template:'+ emailTemplate);
+
---
+++ desarrollandoConNode/src/js-foundation/01-template.js
@@ -0,0 +1,14 @@
+const emailTemplate = `
+<div> 
+<h1>{{name}}</h1>
+<p>Thanks you for order.</p>
+<p>Hola brandon.</p>
+</div>
+`;
+
+console.log(emailTemplate);
+
+module.exports={
+    emailTemplate
+}
+
