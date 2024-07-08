0ababb7dd9e3614559075e74e4c5ce9c0a8ad813
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-07-08 13:59:17 -0500

    usecase

--- 04-MultiplicationApp/src/app.ts
+++ 04-MultiplicationApp/src/app.ts
@@ -1,40 +1,17 @@
-//en este ejercicio estare haciendo la logica para imprimir la tabla del 5 y la imprimiremos con el fs en un txt
-/*import fs from  'fs';
-let outputMessage = '';
-const base = 5;
-const headerMessage = `
-    =======================
-        Tabla del ${base}
-    =======================
-
-
-`;
-
-for (let i = 1; i <= 10; i++) {
-    outputMessage += `${base} x ${i} = ${base * i}\n`;
-}
-
-const impresion = headerMessage + outputMessage;
-
-console.log(impresion);
-
-fs.writeFile('tabla_del_5.txt', impresion, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});	
-
-/*
-fs.writeFile('tabla_del_6.txt'm impresion , (err) =>{
-
-
-
-});
-
-
-*/
-
-import { argv } from "yargs";
 import { yarg } from "./config/plugins/args.plugin";
+import { ServerApp } from "./presentation/server-app";
 
-console.log(process.argv);
-console.log(argv);
+
+(async () => {
+  await main();
+})();
+
+
+async function main() {
+
+  const { b: base, l: limit, s: showTable, n: fileName, d: fileDestination } = yarg;
+
+
+  ServerApp.run({ base, limit, showTable, fileName, fileDestination });
+
+}
